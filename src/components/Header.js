23a8e31649664c1484bd5e00a2cc6179cba9e080
lodestar <122153472+Ld-star0395@users.noreply.github.com>
2023-04-20 01:14:03 +0600

    fix header

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ function Header() {
                     <a href="/" className={videocenter ? "text-decoration-none text-white videoheader" : "text-decoration-none text-white "} ><h1 >michael bloom</h1></a>
                     <div className={videocenter ? "py-4" : "" }></div>
                 </div>
-                <div className="d-flex flex-wrap justify-content-between justify-content-md-center py-2  pt-md-5">
+                <div className="d-flex flex-wrap justify-content-between justify-content-md-center p-2  pt-md-5">
                     <a href="/contact" className="item text-decoration-none">Contact</a>
                     <a href="/projects" className="item text-decoration-none">Personal Projects</a>
                     <a href="/photos" className="item text-decoration-none">Photos</a>
