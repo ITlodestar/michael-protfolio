bb4e3a499e6ebf6402f457409f104d85db10f0fd
lodestar <122153472+Ld-star0395@users.noreply.github.com>
2023-04-20 03:33:01 +0600

    fix photo

--- src/components/Carouselphoto.js
+++ src/components/Carouselphoto.js
@@ -10,7 +10,7 @@ function Carouselphoto({ images, playicon, previewicon }) {
                 className="position-absolute top-50 start-50 translate-middle p-3"
                 href={item.type === "play" ? "" : "/Resume/Untitled Raouls Project.pdf"}
                 target={item.type === "play" ? "" : "_blank"}
-                style={item.type === "play" ? { width: 100, height: 100 } : { width: `100%`, height: `100%` }}
+                style={item.type === "play" ? { width: 100, height: 100 } : { width: `103%`, height: `103%` }}
               >
                 <Image
                   className={item.type === "play" ? "w-100" : "pdf-display w-100"}
