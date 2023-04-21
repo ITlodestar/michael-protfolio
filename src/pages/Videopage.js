e66a89115e88045bcae1dfa1d563e9a60ab1a2d9
lodestar <122153472+Ld-star0395@users.noreply.github.com>
2023-04-22 03:04:47 +0600

    be able to see the home page just slightly at the far right of the page

--- src/pages/Videopage.js
+++ src/pages/Videopage.js
@@ -37,8 +37,8 @@ function Videopage() {
       <h3 className="text-center py-3 v-small-title">michael bloom</h3>
       {
         images.video.map((item) => (
-          <div className="ps-3 d-flex align-items-center title-video-md" key={item.path} >
-            <div className={item.first ? " cursor-pointer section-video me-0 w-100 my-4  " : "cursor-pointer section-video me-5 w-100 my-4"}>
+          <div className="ps-3 d-flex align-items-center title-video-md" style={{backgroundColor: "#000"}} key={item.path} >
+            <div className={item.first ? " cursor-pointer section-video pe-0 w-100 py-4  " : "cursor-pointer section-video w-100 py-4"}>
               <div className="revealUp  large-title"
                 style={{ left: `10%`, bottom: `15%`, zIndex: 1 }}>
                 <h1
